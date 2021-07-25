394c7401eb01283c969bbe9eac593b72c43de06f
Ola Rasmus Moen <ola.moen@wellstarter.com>
2021-07-25 11:58:18 +0200

    hopefully runs

--- parser.py
+++ parser.py
@@ -1,37 +1,50 @@
 import os
+import socket
+import math
+from datetime import datetime
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as dates
+import matplotlib.gridspec as gridspec
 
 
 def plot_data():
-    path = "/Users/olamoen/dev/plot_weatherstation/data/raw/2021/2021-07/"
-    files = os.listdir(path)
-    file = path+files[5] # NEED TO FIT CURRENT DATE TO FILENAME
-    date = files[5].split(".")[0]
+
+    #files = os.listdir(path)
+    #file = path+files[5] # NEED TO FIT CURRENT DATE TO FILENAME
+    file = file_path()
 
     df = pd.read_csv(file, names=["timestamp", "interval", "H_in", "T_in", "H_out", "T_out", "P_abs", "W_avg", "W_gust", "W_dir", "R_hour", "R_year"])
     df.timestamp = pd.to_datetime(df.timestamp, format="%Y-%m-%d %X")
+    hour_avg = df.groupby([df.timestamp.dt.hour]).mean()
 
-    fig, axs = plt.subplots(2, 1)
-    #fig.suptitle(f"Outside temperature: {date}")
+    fig = plt.figure(figsize=(8,5))
+    gs = gridspec.GridSpec(3, 1, height_ratios=[2, 2, 1])
 
-    axs[0].plot(df.timestamp, df.T_out)
-    #axs[0].ylabel("$^\circ$C")
-    axs[0].xaxis.set_major_formatter(dates.DateFormatter('%H:%M'))
-    axs[0].grid()
+    ax1 = fig.add_subplot(gs[0,:])
+    ax1.plot(df.timestamp, df.T_out)
+    ax1.xaxis.set_major_formatter(dates.DateFormatter('%H:%M'))
+    ax1.set_ylabel("Temperatur [$^\circ$C]")
+    ax1.tick_params(bottom=False, labelbottom=False)
+    ax1.grid()
 
-    axs[1].scatter(df.timestamp, df.W_gust, color="black", s=1.5, label="Wind gust")
-    axs[1].plot(df.timestamp, df.W_avg, label="Wind avg.")
-    axs[1].xaxis.set_major_formatter(dates.DateFormatter('%H:%M'))
-    axs[1].legend()
+    ax2 = fig.add_subplot(gs[1,:])
+    ax2.plot(df.timestamp, df.W_avg)
+    ax2.xaxis.set_major_formatter(dates.DateFormatter('%H:%M'))
+    ax2.set_ylabel("Vindstyrke [m/s]")
+    ax2.scatter(df.timestamp, df.W_gust, color="black", s=1.5, label="Vindkast")
+    ax2.legend()
+    ax2.grid()
+
+    wind(hour_avg, fig, gs)
+    gs.update(wspace=0, hspace=0.1)
 
     plt.savefig("today.png")
 
 
-def wind(df):
+def wind(df, fig, gs):
     direction = df.W_dir.astype(float)
     direction *= 22.5
     direction = direction.apply(math.radians)
@@ -39,9 +52,27 @@ def wind(df):
     W_E = direction.apply(math.sin).values
     W_N = direction.apply(math.cos).values
 
-    W_vecs = np.stack((W_E, W_N), axis=-1)
+    ax3 = fig.add_subplot(gs[2,:])
+    ax3.quiver(W_E, W_N)#, scale=1, scale_units="height")
+    ax3.axis("off")
+    ax3.set_ylim([-1, 1])
 
+def file_path():
+    timestamp = datetime.now()
+    year = str(timestamp.year)
+    month = str(timestamp.month)
+    day = str(datetime.now().day)
 
+    y_m = year + "-" + month.rjust(2, "0")
+    y_m_d = y_m + "-" + day.rjust(2, "0")
+
+    hostname = socket.gethostname()
+    if hostname == "Roma":
+        path = "/home/terje/weather/data/raw/"+year+"/"+y_m+"/"+y_m_d+".txt"
+    else:
+        path = "/Users/olamoen/dev/plot_weatherstation/data/raw/"+year+"/"+y_m+"/"+y_m_d+".txt"
+
+    return(path)
 
 if __name__ == "__main__":
     plot_data()
